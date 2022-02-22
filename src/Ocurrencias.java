3541a0b0fa8ff16a644394ec5cdb69b11b5edac7
Pablo <palonsop@alumnos.unex.es>
2022-02-22 18:38:45 +0100

    Versión 2.1 22/02/2022 18:38

--- src/Ocurrencias.java
+++ src/Ocurrencias.java
@@ -3,30 +3,35 @@ import java.util.*;
 
 public class Ocurrencias implements Serializable {
 	
+	Map <String, Integer> ocurr = new TreeMap <String, Integer> ();
 	Integer Ft;
-	Map<String, Integer> ocurr = new TreeMap <String, Integer> ();
 	
+	//Inicializa las instancias de la clase Ocurrencias (constructor).
 	public Ocurrencias (String nf) {
+		ocurr.put (nf, 1);
 		Ft = 1;
-		ocurr.put(nf, 1);
 	}
 	
+	//Devuelve/retorna el atributo "Ft".
 	public Integer getFt () {
 		return Ft;
 	}
+	
+	//Devuelve/retorna el atributo "ocurr".
+	public Map <String, Integer> getOcurr () {
+		return ocurr;
+	}
 
-	public void putOcurr (String pathFichero) {
-		if (ocurr.get(pathFichero) != null) {
-			ocurr.put(pathFichero, ocurr.get(pathFichero) + 1);
+	//Si existe en el mapa "ocurr" la ruta del fichero pasado por parámetro, aumenta en uno el número de palabras que ya tenía. En
+	//caso contrario inicializa ese número de palabras (ftd) a 1, al ser la primera palabra de ese tipo que aparece en ese fichero.
+	public void putOcurr (String nf) {
+		if (ocurr.get (nf) != null) {
+			ocurr.put (nf, ocurr.get (nf) + 1);
 		}
 		else {
-			ocurr.put(pathFichero, 1);
+			ocurr.put (nf, 1);
 		}
 		Ft++;
-	}
-	
-	public Map<String, Integer> getOcurr () {
-		return ocurr;
 	}
 	
 }
--- src/PCCrawling.java
+++ src/PCCrawling.java
@@ -3,103 +3,107 @@ import java.io.*;
 
 public class PCCrawling {
 	
-	static Map<String, Ocurrencias> map;
+	static Map <String, Ocurrencias> map;
 	
-	//Lista los directorios y ficheros que se encuentran en el directorio pasado como parámetro de entrada (args [0]). 
-	public static void listaIt (String [] args) throws Exception {
-		File fichero = new File (args [0]);
+	//Lista los directorios y ficheros que se encuentran en el directorio pasado como parámetro de entrada (rutaEntrada). 
+	public static void listaIt (String rutaEntrada) throws Exception {
+		File fichero = new File (rutaEntrada);
+		
 		if (!fichero.exists () || !fichero.canRead ()) {
-			System.out.println ("[ERROR] El sistema no pudo leer el fichero " + fichero);
+			System.out.println ("[ERROR] El sistema no pudo encontrar el fichero " + fichero);
 			return;
 		}
 		if (fichero.isDirectory ()) {
 			String [] listaFicheros = fichero.list ();
-			String tmpRuta = args [0];
+			String tmpRuta = rutaEntrada;
 			for (int i = 0; i < listaFicheros.length; i++) {
 				System.out.println (" - " + listaFicheros [i]);
-				args [0] = args [0] + "\\" + listaFicheros [i];
-				listaIt (args);
-				args [0] = tmpRuta;
+				listaIt (fichero.getPath () + "\\" + listaFicheros [i]);
+				rutaEntrada = tmpRuta;
 			}
 		} else
 			try {
 				//Es interesante filtrar previamente aquí los ficheros de extensión textual, como: txt, java, p, cpp...
-				fichContPalabras (args);
+				if (rutaEntrada.endsWith (".txt")) {
+					fichContPalabras (rutaEntrada);
+				}
 			} catch (FileNotFoundException fnfe) {
-				System.out.println ("[ERROR] Fichero desaparecido en combate");
+				System.out.println ("[ERROR] El sistema no pudo leer el fichero " + fichero);
 			}
 	}
 
-	//Cuenta el número de veces que aparece cada palabra del fichero pasado por primer parámetro (args [0]) y vuelca
-	//el resultado en el fichero "RIBW_actividad01_salida" en la ruta pasada como segundo parámetro (args [1]).
-	public static void fichContPalabras (String args []) throws IOException {
-		String fichEntrada = args [0];
-		String fichSalida = args [1];
-		File fPath = new File (args [0]);
+	//Cuenta el número de veces que aparece cada palabra en el fichero pasado como parámetro de entrada (rutaEntrada).
+	public static void fichContPalabras (String rutaEntrada) throws IOException {
 
-		BufferedReader br = new BufferedReader (new FileReader (fichEntrada));
+		BufferedReader br = new BufferedReader (new FileReader (rutaEntrada));
+		File fPath = new File (rutaEntrada);
 		String linea;
 
 		while ((linea = br.readLine ()) != null) {
 			StringTokenizer st = new StringTokenizer (linea, ";:¡!¿?{}[]().,/\\_- \n\r\"'");
 			while (st.hasMoreTokens ()) {
-				String s = st.nextToken ().toLowerCase();
+				String s = st.nextToken ().toLowerCase ();
 				Object o = map.get (s);
 				if (o == null) {
-					map.put (s, new Ocurrencias (fPath.getPath())); //args[0]
+					map.put (s, new Ocurrencias (fPath.getPath ()));
 				}
 				else {
-					Ocurrencias oc = map.get(s);
-					oc.putOcurr(fPath.getPath());
-					map.put (s, oc);
-					oc = map.get(s);
+					((Ocurrencias) o).putOcurr (fPath.getPath ());
+					map.put (s, (Ocurrencias) o);
 				}
 			}
 		}
 		br.close ();
-
-		List<String> claves = new ArrayList<String> (map.keySet ());
+	}
+	
+	//Vuelca el resultado en el fichero "RIBW_salida" en la ruta pasada como parámetro de entrada ("rutaSalida").
+	public static void salvarSalida (String rutaSalida) throws IOException {
+		List <String> claves = new ArrayList <String> (map.keySet ());
 		Collections.sort (claves);
 		
-		PrintWriter pr = new PrintWriter (new FileWriter (fichSalida + "\\RIBW_actividad01_salida.txt"));
-		Iterator<String> i = claves.iterator ();
+		PrintWriter pr = new PrintWriter (new FileWriter (rutaSalida + "\\RIBW_salida.txt"));
+		Iterator <String> i = claves.iterator ();
 		while (i.hasNext ()) {
 			Object k = i.next ();
-			Ocurrencias oc = map.get(k);
-			pr.println (k + " : " + oc.getFt());
-			Map <String, Integer> aux = oc.getOcurr();
-			List<String> l = new ArrayList<String> (aux.keySet());
-			Iterator<String> it = l.iterator();
-			while (it.hasNext()) {
-				Object s = it.next();
-				pr.println("\t--> " + s + " : " + aux.get(s));
+			Ocurrencias oc = map.get (k);
+			pr.println (k + " : " + oc.getFt ());
+			
+			Map <String, Integer> aux = oc.getOcurr ();
+			List <String> l = new ArrayList <String> (aux.keySet ());
+			Iterator <String> it = l.iterator ();
+			while (it.hasNext ()) {
+				Object s = it.next ();
+				pr.println ("\t- " + s + " : " + aux.get (s));
 			}
 		}
+		
+		System.out.println ("[SALVADO] El sistema salvó el archivo especificado (RIBW_salida.txt)");
 		pr.close ();
 	}
 
-	//Salva el objeto de tipo "TreeMap" (Map) llamado "map.ser" en la ubicación pasada como segundo parámetro (args [1]).
-	public static void salvarObjeto (String args []) {
+	//Salva el objeto de tipo "TreeMap" (Map) en el fichero "map.ser" de la ubicación pasada como parámetro ("rutaSalida").
+	public static void salvarObjeto (String rutaSalida) {
 		try {
-			FileOutputStream fos = new FileOutputStream (args[1] + "\\map.ser");
+			FileOutputStream fos = new FileOutputStream (rutaSalida + "\\map.ser");
 			ObjectOutputStream oos = new ObjectOutputStream (fos);
 			oos.writeObject (map);
 			System.out.println ("[SALVADO] El sistema salvó el archivo especificado (map.ser)");
+			oos.close ();
 		} catch (Exception e) {
 			System.out.println ("[ERROR] El sistema no pudo salvar el archivo especificado (map.ser)");
 			System.out.println (e);
 		}
 	}
 
-	//Carga el fichero "h.ser" que se encuentra en la ruta pasada como segundo parámetro de entrada (args [1]) y lo retorna
+	//Carga el fichero "map.ser" que se encuentra en la ruta pasada como parámetro de entrada ("rutaSalida") y lo retorna
 	//como un objeto de tipo "TreeMap" (Map). En caso de que no encuentre "map.ser" arroja una excepción y devuelve "null".
-	public static Map<String, Ocurrencias> cargarObjeto (String args []) {
+	public static Map <String, Ocurrencias> cargarObjeto (String rutaSalida) {
 		try {
-			FileInputStream fis = new FileInputStream (args[1] + "\\map.ser");
+			FileInputStream fis = new FileInputStream (rutaSalida + "\\map.ser");
 			ObjectInputStream ois = new ObjectInputStream (fis);
-			map = (TreeMap<String, Ocurrencias>) ois.readObject ();
-			ois.close();
+			map = (TreeMap <String, Ocurrencias>) ois.readObject ();
 			System.out.println ("[CARGA] El sistema cargó el archivo especificado (map.ser)");
+			ois.close ();
 			return map;
 		} catch (Exception e) {
 			System.out.println ("[ERROR] El sistema no pudo cargar el archivo especificado (map.ser)");
@@ -110,23 +114,26 @@ public class PCCrawling {
 	//Inicia la ejecución del programa Java y debe contener dos parámetros de entrada: args [0] hace referencia al directorio que
 	//se quiere recorrer y por otro lado, args [1] hace referencia al directorio de salida donde se volcará las palabras contadas.
 	public static void main (String [] args) throws Exception {
-		File f1 = new File (args[0]);
-		File f2 = new File (args[1]);
-		if (args.length < 2 && !f1.canRead() && !f2.canRead()) {
+		File f1 = new File (args [0]);
+		File f2 = new File (args [1]);
+		
+		if (args.length < 2 && !f1.canRead () && !f2.canRead ()) {
 			System.out.println ("[ERROR] Formato: > java PCCrawling directorio_entrada directorio_entrada_salida");
 		}
 		else {
 			System.out.println ("[INICIO] El programa ha iniciado correctamente");
-			File file = new File (args[1] + "\\map.ser"); 
+			File file = new File (args [1] + "\\map.ser"); 
 
 			if (file.exists ()) {
-				map = cargarObjeto (args);
+				map = cargarObjeto (args [1]);
 			} else {
 				System.out.println ("[CARGA] El sistema ha creado el archivo especificado (map.ser)");
-				map = new TreeMap<String, Ocurrencias> ();
+				map = new TreeMap <String, Ocurrencias> ();
 			}
-			listaIt (args);
-			salvarObjeto (args);
+		
+			listaIt (args [0]);
+			salvarSalida (args [1]); //Salva el fichero de salida (RIBW_salida.txt).
+			salvarObjeto (args [1]); //Salva el objeto/diccionario de salida (map.ser).
 			System.out.println ("[FINAL] El programa ha finalizado correctamente");
 		}
 	}
